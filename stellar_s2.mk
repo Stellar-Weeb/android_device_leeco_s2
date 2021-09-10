81271e0c07dc4b76773f489b0cade91b366712a0
1prime1 <87894324+1prime1@users.noreply.github.com>
2021-09-10 17:28:36 +0530

    s2: stellar implementation

--- stellar_s2.mk
+++ stellar_s2.mk
@@ -37,5 +37,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=Le2_WW \
     PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001.A1 7511028 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
 BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
